d8aa35a59bfdc830c81e736ac95ec3ebd547fc3c
Faisal A F Rahman <afaisalalmunawar0315@gmail.com>
2023-11-09 19:45:17 +0700

    add searchResto

--- src/scripts/global/favorite-resto.js
+++ src/scripts/global/favorite-resto.js
@@ -25,6 +25,18 @@ const favoriteRestoDb = {
   async deleteResto(id) {
     return (await dbPromise).delete(OBJECT_STORE_APP, id);
   },
+
+  async searchResto(query) {
+    return (await this.getAllResto()).filter((resto) => {
+      const loweredCaseRestoTitle = (resto.title || '-').toLowerCase();
+      const jammedRestoTitle = loweredCaseRestoTitle.replace(/\s/g, '');
+
+      const loweredCaseQuery = query.toLowerCase();
+      const jammedQuery = loweredCaseQuery.replace(/\s/g, '');
+
+      return jammedRestoTitle.indexOf(jammedQuery) !== -1;
+    });
+  },
 };
 
 export default favoriteRestoDb;
